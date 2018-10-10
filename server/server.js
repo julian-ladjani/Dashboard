087b2d6403051414a82a1cbda1b7bb3de6835a52
lucas.depret <lucas.depret@epitech.eu>
2018-10-10 18:11:10 +0200

    [server] PokeApi

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs');
+var PokeApi = require('pokedex-promise-v2');
+var Pokedex = new PokeApi();
 
 // Constants
 const PORT = process.env.PORT || 8080;
@@ -9,7 +12,20 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hewllo world\n');
+  res.status(200).send('Hello world\n');
+});
+
+app.get('/about.json', (req, res) => {
+  res.header('Content-Type','text/html');
+  res.status(200).send("lol");
+});
+
+app.get('/poketest', (req, res) => {
+  res.header('Content-Type','text/html');
+  Pokedex.getPokemonByName(parseInt(Math.random()*801+1)) // with Promise
+    .then(function(response) {
+      res.status(200).send("<img src='"+response.sprites.front_shiny+"'>"+"<h1>"+response.forms[0].name+"</h1>");
+    })
 });
 
 app.listen(PORT, HOST);
