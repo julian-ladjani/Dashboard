6b3332d8346c666671445316beecea2a446b5934
yannick <yannick.suc@epitech.eu>
2018-10-13 16:10:24 +0200

    [client] Add toute's dependencies

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: true }
+      ),
+      BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
