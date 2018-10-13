40b5f714785ef8fd16f00c784d1e9870be7b41bc
julian ladjani <julian.ladjani@epitech.eu>
2018-10-13 15:38:46 +0200

    [Client] Remake client

--- server/src/api/routes/pokemon.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(router) {
-    const pokemonApi = require('../controllers/pokemon');
-
-    // todoList Routes
-    router
-        .get('/pokemon', pokemonApi.get_random_pokemon);
-};
