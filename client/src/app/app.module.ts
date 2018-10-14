bba516b86823079b82d875270d4613ba9a230325
yannick <yannick.suc@epitech.eu>
2018-10-14 13:07:41 +0200

    [client] Add login componeenten basic routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
+import {AngularSvgIconModule} from 'angular-svg-icon';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { LoginPageComponent } from './pages/login/login-page.component';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+    { path: '',
+        redirectTo: '/login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        component: LoginPageComponent
+    }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     RouterModule.forRoot(
         appRoutes,
         { enableTracing: true }
       ),
-      BrowserModule
+      HttpClientModule,
+      BrowserModule,
+      AngularSvgIconModule,
+      NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
