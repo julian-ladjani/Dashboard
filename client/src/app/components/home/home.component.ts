0b132e76a9a3871ec3900fa79475622066e0270e
gregory.eple <gregory.eple@localhost.localdomain>
2018-10-16 21:41:30 +0200

    [CLient] added logout button

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, ComponentFactoryResolver, OnInit} from '@angular/core';
 import {WidgetContainerComponent} from '../widgets/widget-container.component';
 import {MatDialog} from '@angular/material';
 import {WidgetService} from '../../services/widget.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ import {WidgetService} from '../../services/widget.service';
 
 export class HomeComponent implements OnInit {
 
-  constructor(public matDialog: MatDialog, public factory: WidgetService) { }
+  constructor(public matDialog: MatDialog, public factory: WidgetService, private router: Router) { }
   widgets: Array<{String}> = [];
   resolver: ComponentFactoryResolver;
 
@@ -20,5 +21,10 @@ export class HomeComponent implements OnInit {
 
   addWidget(name) {
     this.widgets.push(name);
+  }
+
+  logout() {
+      window.localStorage.setItem('token', '');
+      this.router.navigateByUrl('/login');
   }
 }
