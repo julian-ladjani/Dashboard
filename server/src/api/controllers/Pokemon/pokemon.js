2ccab76d55c432a467294ba967cb5c537602084a
depret-l <lucas.depret@epitech.eu>
2018-10-17 14:13:13 +0200

    [server] pokemon route default

--- server/src/api/controllers/Pokemon/pokemon.js
+++ server/src/api/controllers/Pokemon/pokemon.js
@@ -8,19 +8,25 @@ exports.auth = function(req,res) {
 };
 
 exports.getPokemon = function(req, res) {
+    var index = req.params.shiney === undefined ? 0 : req.params.shiney;
     Pokedex.getPokemonByName(req.params.poke) // with Promise
         .then(function(response) {
+            var sprites = [response.sprites.front_default, response.sprites.front_shiny];
             var tmp = {'pokemon':{name:pokemon.getName(response.game_indices[0].game_index,req.params.lang),
-                    sprites:[response.sprites]}};
+                    sprites:sprites[index]}};
             res.send(tmp);
         });
 };
 
 exports.getBlind = function(req, res) {
-    Pokedex.getPokemonByName(parseInt(Math.random()*req.params.gen+1)) // with Promise
+    var indexSprite = req.params.shiney === undefined ? 0 : req.params.shiney;
+    var indexGen = req.params.gen === undefined ? 802 : req.params.gen;
+    var indexLang = req.params.lang === undefined ? 'en' : req.params.lang;
+    Pokedex.getPokemonByName(parseInt(Math.random()*indexGen+1)) // with Promise
         .then(function(response) {
-            var tmp = {'pokemon':{name:pokemon.getName(response.game_indices[0].game_index,req.params.lang),
-                    sprites:[response.sprites]}, 'time':10};
+            var sprites = [response.sprites.front_default, response.sprites.front_shiny];
+            var tmp = {'pokemon':{name:pokemon.getName(response.game_indices[0].game_index, indexLang),
+                    sprites:sprites[indexSprite]}, 'time':10};
             res.send(tmp);
         });
 };
