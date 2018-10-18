ada07bbcb4bcf013538d5c3ad6861e543cee346d
depret-l <lucas.depret@epitech.eu>
2018-10-18 17:58:40 +0200

    [server] road to epitech api

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -4,11 +4,12 @@ const router = require('express').Router();
 const pokemonRouter = require('./Pokemon/pokemon');
 const weatherRouter = require('./Weather/weather');
 const spotifyRouter = require('./Spotify/spotifyApi');
+const epitechRouter = require('./Epitech/epitech');
 const auth = require('./auth/auth');
 
 router.use('/pokemon', pokemonRouter);
 router.use('/auth', auth);
 router.use('/weather', weatherRouter);
 router.use('/spotify', spotifyRouter);
-
+router.use('/epitech', epitechRouter);
 module.exports = router;
