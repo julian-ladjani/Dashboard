15ce982d3b9ac1a49bd64ca126214d222372b1a5
depret-l <lucas.depret@epitech.eu>
2018-10-21 15:41:51 +0200

    [server wigget] final drop planning

--- server/src/api/controllers/epitech/planning.js
+++ server/src/api/controllers/epitech/planning.js
@@ -2,26 +2,36 @@
 
 const _ = require('lodash');
 const request = require('request');
+const moment = require('moment');
 
 exports.getWidgetInfo = async function(params) {
     if (!_.hasIn(params, 'autologin') || params.autologin == null || !_.hasIn(params, 'begin')
-        || !_.hasIn(params, 'end') || !_.hasIn(params, 'semester'))
+        || !_.hasIn(params, 'days') || !_.hasIn(params, 'semester'))
         return false;
     if (Date(params.begin) < Date())
         params.begin = moment().format('YYYY-MM-DD');
-    if (Date(params.end) < Date())
-        params.end = moment().format('YYYY-MM-DD').add('days', 1);
+    params.days = params.days < 0 ? 0 : params.days;
+    let end = moment(params.begin).add(params.days, 'days').format('YYYY-MM-DD');
     if (params.semester.length === 0)
         params.semester.push(0);
     return new Promise(function (resolve, reject) {
         request('https://intra.epitech.eu/' + params.autologin + '/planning/load?start=' + params.begin + '&end='
-            + params.end + '&format=json',
+            + end + '&format=json',
             function (err, responce, body) {
-                if (err)
-                    reject(err);
-                console.log(body);
-                let json = JSON.parse(body);
-                resolve(json);
+                if (err) {
+                    reject(false);
+                    return false;
+                }
+                let intraObj = JSON.parse(body);
+                if (intraObj === undefined || intraObj === {}) {
+                    reject(false);
+                    return(false);
+                }
+                let planning = Object.values(intraObj).filter(
+                    el => params.semester.find(function(x) {
+                    return (el.semester === x || el.semester === 0);})
+                );
+                resolve(planning);
             })
     })
 };
